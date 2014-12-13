1201647accdea3abec8ca6cd2d26557a580afe04
Yishen Chen <cyt046@gmail.com>
2014-12-13 16:33:08 -0600

    flush leftover place at the end of scanning

--- load-places.go
+++ load-places.go
@@ -43,6 +43,20 @@ func check(e error) {
 	}
 }
 
+func dbExecAndCheck(db *sql.DB, stmt string, args ...interface{}) {
+	_, err := db.Exec(stmt, args...)
+	check(err)
+}
+
+func getInsertStmt(insertCount int) string {
+	placeHolders := make([]string, insertCount)
+	for i := range placeHolders {
+		placeHolders[i] = "(?,?,?,?,?,?)"
+	}
+
+	return INSERT_STMT + strings.Join(placeHolders, ",")
+}
+
 func main() {
 	curDir, err := os.Getwd()
 	check(err)
@@ -57,16 +71,9 @@ func main() {
 	defer db.Close()
 	check(err)
 
-	db.Exec(DROP_STMT)
-	db.Exec(CREATE_STMT)
-	db.Exec(INDEX_STMT)
-
-	placeHolders := make([]string, VAL_COUNT)
-	for i := range placeHolders {
-		placeHolders[i] = "(?,?,?,?,?,?)"
-	}
-
-	insert := INSERT_STMT + strings.Join(placeHolders, ",")
+	dbExecAndCheck(db, DROP_STMT)
+	dbExecAndCheck(db, CREATE_STMT)
+	dbExecAndCheck(db, INDEX_STMT)
 
 	f, err := os.Open(config.Input)
 	defer f.Close()
@@ -75,29 +82,35 @@ func main() {
 	reader := bufio.NewReader(f)
 	scanner := bufio.NewScanner(reader)
 	counter := 0
-	var vals []interface{}
+	vals := make([]interface{}, 0, VAL_COUNT*6)
 
 	fmt.Println("loading geodata into database...")
-
 	for scanner.Scan() {
-		if counter%VAL_COUNT == 0 {
-			if counter > 0 {
-				_, e := db.Exec(insert, vals...)
-				if e != nil {
-					fmt.Println(e)
-				}
-			}
-
+		if counter > 0 && counter%VAL_COUNT == 0 {
+			dbExecAndCheck(db,
+				getInsertStmt(VAL_COUNT),
+				vals...)
 			vals = make([]interface{}, 0, VAL_COUNT*6)
 		}
 
 		placeData := strings.Split(scanner.Text(), "\t")
-		if len(placeData) < 6 {
-			continue
+		if len(placeData) == 6 {
+			vals = append(vals,
+				placeData[0],
+				placeData[1],
+				placeData[2],
+				placeData[3],
+				placeData[4],
+				placeData[5])
+			counter++
 		}
-		vals = append(vals, placeData[0], placeData[1], placeData[2], placeData[3], placeData[4], placeData[5])
+	}
 
-		counter++
+	if counter%VAL_COUNT > 0 {
+		fmt.Println(counter % VAL_COUNT)
+		dbExecAndCheck(db,
+			getInsertStmt(counter%VAL_COUNT),
+			vals...)
 	}
 
 	fmt.Println("Finished. Loaded", counter, "places...")
